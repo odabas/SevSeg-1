75ab4e494e00039927ee2132cb93adfa712eb1d1
odabas <tr.tc@msn.com>
2016-12-31 14:57:23 +0200

    Update SevSeg.cpp

--- SevSeg.cpp
+++ SevSeg.cpp
@@ -533,11 +533,12 @@ void SevSeg::setDigitCodes(byte digits[], char decPlaces) {
       }
     }
   }
-}
+
 void SevSeg::beep(int delayms){
   tone(A0, 1000);      // turn it on, pin and value
   delay(delayms);          // wait for a delayms ms
   noTone(A0);       // off
   delay(delayms);          // wait for a delayms ms
 }  
+  }
 /// END ///
